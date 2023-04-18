9b8736f99c9c3476a85a9927a2e1070fa0f15f24
ramonprog <programador.ramon@gmail.com>
2023-04-18 15:47:43 -0300

    initial setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return `World`;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 HTTP server running on ${url}`);
+});
